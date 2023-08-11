8eec72d60fb1119ec04820281e34df1947eef551
Yoon Cho <yoon@Yoons-Air.lan>
2023-08-11 14:57:19 -0700

    lesson 8 completed

--- Lesson07and08/build/js/main.js
+++ Lesson07and08/build/js/main.js
@@ -52,3 +52,127 @@ const monthlyIncomes = {
 for (const rev in monthlyIncomes) {
     console.log(monthlyIncomes[rev]);
 }
+/////////////////////////////////////////////
+// Lesson 8 Generics: <T> is an example of type variable/parameter
+// Type parameter is a feature that allows you to create generic types or functions that can work with a variety of data types
+console.log("///////////// Lesson 8 /////////////");
+const echo = (arg) => arg;
+const isObj = (arg) => {
+    // array and null returns as a type of object, so need to take account for that
+    return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
+};
+console.log(isObj(true));
+console.log(isObj("John"));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: "John" }));
+console.log(isObj(null));
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
+};
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue("dave"));
+console.log(isTrue(""));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({})); // modified
+console.log(isTrue({ name: "dave" }));
+console.log(isTrue([])); // modified
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+const checkBoolValue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return { value: arg, is: !!arg };
+};
+const processUser = (user) => {
+    // process the user with logic here
+    return user;
+};
+console.log(processUser({ id: 1, name: "dave" }));
+// console.log(processUser({name: 'dave'})); // will throw an error because id is required
+const getUsersProperty = (users, key) => {
+    return users.map((user) => user[key]);
+};
+const usersArray = [
+    {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        address: {
+            street: "Kulas Light",
+            suite: "Apt. 556",
+            city: "Gwenborough",
+            zipcode: "92998-3874",
+            geo: {
+                lat: "-37.3159",
+                lng: "81.1496",
+            },
+        },
+        phone: "1-770-736-8031 x56442",
+        website: "hildegard.org",
+        company: {
+            name: "Romaguera-Crona",
+            catchPhrase: "Multi-layered client-server neural-net",
+            bs: "harness real-time e-markets",
+        },
+    },
+    {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv",
+        address: {
+            street: "Victor Plains",
+            suite: "Suite 879",
+            city: "Wisokyburgh",
+            zipcode: "90566-7771",
+            geo: {
+                lat: "-43.9509",
+                lng: "-34.4618",
+            },
+        },
+        phone: "010-692-6593 x09125",
+        website: "anastasia.net",
+        company: {
+            name: "Deckow-Crist",
+            catchPhrase: "Proactive didactic contingency",
+            bs: "synergize scalable supply-chains",
+        },
+    },
+];
+console.log(getUsersProperty(usersArray, "email"));
+console.log(getUsersProperty(usersArray, "username"));
+class StateObj {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
+}
+const store = new StateObj("John");
+console.log(store.state);
+store.state = "Dave";
+// store.state = 12
+const myState = new StateObj([15]);
+console.log(myState.state);
+myState.state = ["dave", 18, true];
+console.log(myState.state);
